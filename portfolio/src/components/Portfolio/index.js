fa8ed9eec71468b4ca9a6d414d9a2fb79ea01637
Jonathan W <jwilliams12117@gmail.com>
2022-05-07 13:59:00 -0700

    Added structure to map project array to portfolio

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Portfolio() {
+
+    // Define projects array for use in mapping to the component
+    const projects = [
+        {
+            title: "Project 1",
+            imgPath: "../../assets/images/filename.jpg",
+            imgAlt: "Description of this image's content",
+            description: "Description of this project.  We used Javascript.",
+            gitHubLink: "https://github.com/LumberJon1/",
+            deploymentLink: "https://google.com"
+        },
+        {
+            title: "Project 2",
+            imgPath: "../../assets/images/filename.jpg",
+            imgAlt: "Description of this image's content",
+            description: "Description of this project.  We used HTML.",
+            gitHubLink: "https://github.com/LumberJon1/",
+            deploymentLink: "https://google.com"
+        },
+    ]
 
     // return render
     return (
         <section>
-            // Map to render out list of projects from sister component
+            <h1>My Projects</h1>
+            {projects.map((project) => (
+                <div>
+                    <h2>{project.title}</h2>
+                    <img
+                        src={project.imgPath}
+                        alt={project.imgAlt}
+                        className=""
+                    />
+                    <p>{project.description}</p>
+                    <div>
+                        <a href={project.gitHubLink}>View Project Repo</a>
+                        <a href={project.deploymentLink}>View Project Deployment</a>
+                    </div>
+                    {/* key={project.title} */}
+                </div>
+            ))}
         </section>
     )
 }
