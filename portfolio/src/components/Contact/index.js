fd3795c170d486aed76404c11c51eea9ffa6f518
Jonathan W <jwilliams12117@gmail.com>
2022-05-07 13:33:17 -0700

    Added sections to components for visual confirmation

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -3,6 +3,17 @@ import React from "react";
 function Contact() {
 
     // Render return
+    return(
+        <section>
+            <h1>Contact Me</h1>
+            <div>
+                <form action="">
+                    // Form Inputs
+                </form>
+            </div>
+        </section>
+
+    )
 }
 
 export default Contact;
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -3,6 +3,12 @@ import React from "react";
 function Resume() {
 
     // Render return
+    return(
+        <section>
+            <h1>My Resume</h1>
+            // Resume goes here
+        </section>
+    )
 }
 
 export default Resume;
