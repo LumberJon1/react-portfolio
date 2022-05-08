d59b74464ab3002fe52cae06fc24a58c15fadd60
Jonathan W <jwilliams12117@gmail.com>
2022-05-07 19:42:16 -0700

    Added mapped object array to the Projects component and pulled images/links

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,25 +1,48 @@
 import React, {useState} from "react";
 
-
 function Portfolio() {
 
     // Define projects array for use in mapping to the component
     const projects = [
         {
-            title: "Project 1",
-            imgPath: "../../assets/images/filename.jpg",
-            imgAlt: "Description of this image's content",
-            description: "Description of this project.  We used Javascript.",
-            gitHubLink: "https://github.com/LumberJon1/",
-            deploymentLink: "https://google.com"
+            title: "My Pantry",
+            fileNum: 2,
+            imgAlt: "A picture of this project",
+            description: "My Pantry allows you to create a virtual food pantry to see what you've got on the shelves at home, remove items so you'll never have to guess what you're out of, and shop confidently with the knowledge you can check back without standing in front of the cupboard.",
+            gitHubLink: "https://github.com/MagnificentRoast/My-Pantry",
+            deploymentLink: "https://whispering-tundra-55246.herokuapp.com/"
         },
         {
-            title: "Project 2",
-            imgPath: "../../assets/images/filename.jpg",
-            imgAlt: "Description of this image's content",
-            description: "Description of this project.  We used HTML.",
-            gitHubLink: "https://github.com/LumberJon1/",
-            deploymentLink: "https://google.com"
+            title: "Pokemon Randomizer",
+            fileNum: 3,
+            imgAlt: "A picture of this project",
+            description: "Pokemon Randomizer allows you to access the vast Pokemon API to display images of pokemon with a twist: their typing will be randomized.  Become inspired by reimagining pokemon with a completely different type.",
+            gitHubLink: "https://github.com/LumberJon1/Pokemon-Generator",
+            deploymentLink: "https://lumberjon1.github.io/Pokemon-Generator/"
+        },
+        {
+            title: "Workday Scheduler",
+            fileNum: 4,
+            imgAlt: "A picture of this project",
+            description: "Work Day Scheduler is an app that allows you to add tasks to each block of the work day, and will highlight each block of time based on the current hour.",
+            gitHubLink: "https://github.com/LumberJon1/Workday-Scheduler",
+            deploymentLink: "https://lumberjon1.github.io/Workday-Scheduler/"
+        },
+        {
+            title: "City Weather Dashboard",
+            fileNum: 1,
+            imgAlt: "A picture of this project",
+            description: "The City Weather Dashboard app lets you type in a city and see its current and five-day forecast, as well as select from a list of previously-searched cities.",
+            gitHubLink: "https://github.com/LumberJon1/City-Weather-App",
+            deploymentLink: "https://lumberjon1.github.io/City-Weather-App/"
+        },
+        {
+            title: "Check Yourself",
+            fileNum: 0,
+            imgAlt: "A picture of this project",
+            description: "Check Yourself is a site that allows users to keep a virtual journal describing and rating their mood, and based on their scores will suggest local events or activities to do.",
+            gitHubLink: "https://github.com/Jashcraft/check-yourself",
+            deploymentLink: "https://jashcraft.github.io/check-yourself/"
         },
     ]
 
@@ -31,17 +54,17 @@ function Portfolio() {
                 <div>
                     <h2>{project.title}</h2>
                     <img
-                        src={project.imgPath}
+                        src={require(`../../assets/images/${project.fileNum}.jpg`)}
                         alt={project.imgAlt}
                         className=""
-                    />
+                        />
                     <p>{project.description}</p>
                     <div>
                         <a href={project.gitHubLink}>View Project Repo</a>
                         <a href={project.deploymentLink}>View Project Deployment</a>
                     </div>
-                    {/* key={project.title} */}
                 </div>
+                        // key={project.title}
             ))}
         </section>
     )
